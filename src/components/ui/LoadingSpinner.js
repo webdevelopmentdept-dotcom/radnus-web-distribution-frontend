d1e011cbdae25c866da4130018972558b307fdcd
webdevelopmentdept-dotcom <Webdevloepement@gmail.com>
2026-07-14 12:06:51 +0530

    Update Lodaing spinner code

---
+++ src/components/ui/LoadingSpinner.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './LoadingSpinner.css';
+
+const LoadingSpinner = ({ message = 'Loading products...' }) => {
+  return (
+    <div className="loading-container">
+      <div className="loading-spinner">
+        <div className="spinner-ring"></div>
+        <div className="spinner-ring"></div>
+        <div className="spinner-ring"></div>
+        <div className="spinner-ring"></div>
+      </div>
+      <p className="loading-message">{message}</p>
+    </div>
+  );
+};
+
+export default LoadingSpinner;
